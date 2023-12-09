158882d1955c320ae8a92e8d7e70fd4bc5503a65
Mbithi Raymond Kombo <121002402+Komboray@users.noreply.github.com>
2023-12-09 10:55:18 +0300

    Update server1.py

---
+++ server1.py
@@ -0,0 +1,101 @@
+import socket
+import threading
+
+HOST = '127.0.0.1' #localhost
+PORT = 5050
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+
+clients = []
+nicknames = []
+#this is the message being sent to the clients
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            msg = message = client.recv(1024)
+            if msg.decode('ascii').startswith('KICK'):
+                if nicknames[clients.index(client)] == 'admin':
+                    name_to_kick = msg.decode('ascii')[5:]
+                    kick_user(name_to_kick)
+                else:
+                    client.send('Command was refused'.encode('ascii'))
+            elif msg.decode('ascii').startswith('BAN'):
+                if nicknames[clients.index(client)] == 'admin':
+                    name_to_ban = msg.decode('ascii')[4:]
+                    kick_user(name_to_ban)
+                    with open('dumy.txt', 'a') as f:
+                        f.write(f'{name_to_ban}\n')
+                    print(f'{name_to_ban} was banned!')
+                else:
+                    client.send('Command was refused'.encode('ascii'))
+            else:
+                broadcast(message)
+        except:
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f'{nickname} left the chat!'.encode('ascii'))
+                nicknames.remove(nickname)
+                break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send("RAY".encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+
+        with open('dumy.txt', 'r') as f:
+            bans = f.readlines()
+
+        if nickname+'\n' in bans:
+            client.send('BAN'.encode('ascii'))
+            client.close()
+            continue
+
+        if nickname == 'admin':
+            client.send('PASS'.encode('ascii'))
+            password = client.recv(1024).decode('ascii')
+
+            if password != 'adminpass':
+                client.send('REFUSE'.encode('ascii'))
+                client.close()
+                #if we continue, we are going to skip this loop
+                continue
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of client is {nickname}")
+        broadcast(f"{nickname} joined the chat".encode('ascii'))
+        client.send("Connected to the server".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+def kick_user(name):
+    if name is nicknames:
+        name_index = nicknames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send('You were kicked by an admin!'.encode('ascii'))
+        client_to_kick.close()
+        nicknames.remove(name)
+        broadcast(f'{name} was kicked by an admin!'.encode('ascii'))
+
+print("Server is listening.............")
+receive()
+
+# this is a server that holds communication btwn different clients in a chat
+
